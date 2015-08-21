90611e627a353187e2e7ad33332b927184716374
sectore <info@websector.de>
2015-08-21 10:57:33 +0200

    refactoring based on RxJS event emitter example

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,45 +1,83 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-// Based on Angular 2's [EventEmitter](https://github.com/angular/angular/blob/master/modules/angular2/src/facade/async.ts#L98)
-
 import * as Rx from 'rx';
 
+/**
+ * EventEmitter
+ * Based on "How do I create a custom event emitter?"
+ * @see https://github.com/Reactive-Extensions/RxJS/blob/master/doc/howdoi/eventemitter.md
+ */
+class EventEmitter {
 
-export interface Observable {
-  observer(generator: any): Rx.IDisposable;
+    /**
+     * Hash map of subjects
+     * @type {{Rx.Subject}}
+     */
+    private subjects: { [key: string]: Rx.Subject<any> } = {};
+
+    constructor() {}
+
+    /**
+     * Emits events through a subject to all subscribed broadcaster
+     * @param name {string} Name of an event
+     * @param data {Object} Event data
+     */
+    next(name:string, data:Object) {
+        this.subjects[name] || (this.subjects[name] = new Rx.Subject());
+        this.subjects[name].onNext(data);
+    }
+
+    /**
+     * Subscribes a listener to an event.
+     * @param name {string} Name of an event
+     * @param handler {any} Callback of the listener (subscriber)
+     * @returns {Rx.IDisposable}
+     */
+    subscribe(name:string, handler: any):Rx.IDisposable {
+        this.subjects[name] || (this.subjects[name] = new Rx.Subject());
+        return this.subjects[name].subscribe(handler);
+    }
+
+    /**
+    * Indicates whether the subject has observers subscribed to it.
+    * @returns {boolean} Returns true if the Subject has observers, else false.
+    */
+    hasObservers():boolean{
+        return this.subjects[name] && this.subjects[name].hasObservers();
+    }
+
+    /**
+     * Clean up a certain subscriber from a subject
+     * and removes the subject from subject map
+     */
+    dispose(name:string) {
+        if (this.subjects[name]) {
+            this.subjects[name].dispose();
+            delete this.subjects[name];
+        }
+    }
+
+    /**
+     * Clean up all subscribers from subjects
+     * and removes all subjects form subject map
+     */
+    disposeAll() {
+        var subjects = this.subjects;
+        var hasOwnProp:Function = {}.hasOwnProperty;
+        for (var prop in subjects) {
+            if (hasOwnProp.call(subjects, prop)) {
+                subjects[prop].dispose();
+            }
+        }
+
+        this.subjects = {};
+    }
 }
 
-export default class EventEmitter implements Observable {
+/**
+ * Instance of EventEmitter to provide a Singleton
+ * @type {EventEmitter}
+ */
+var emitter: EventEmitter = new EventEmitter();
 
-  private _subject: Rx.Subject<any>;
-  private _immediateScheduler;
-
-  constructor() {
-    this._subject = new Rx.Subject<any>();
-    this._immediateScheduler = (<any>Rx.Scheduler).immediate;
-  }
-
-  observer(generator: any): Rx.IDisposable {
-    return this._subject.observeOn(this._immediateScheduler)
-        .subscribe((value) => { setTimeout(() => generator.next(value)); },
-                   (error) => generator.throw ? generator.throw(error) : null,
-                   () => generator.return ? generator.return () : null);
-  }
-
-  toRx(): Rx.Observable<any> {
-      return this._subject;
-  }
-
-  next(value: any) {
-      this._subject.onNext(value);
-  }
-
-  throw(error: any) {
-      this._subject.onError(error);
-  }
-
-  return (value?: any) {
-      this._subject.onCompleted();
-  }
-
-}
+export default emitter;
